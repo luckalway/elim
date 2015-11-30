0865329a920a16d32128ed2f9b9052cfe09aafb6
malachi.ye <malachi.ye@perficient.com>
2015-11-30 22:19:36 +0800

    slide show play and close

--- framework/simple-slideshow/js/slideshow.js
+++ framework/simple-slideshow/js/slideshow.js
@@ -1,12 +1,12 @@
 $.fn.extend({
-			gallerySlide : function() {
-				var images = $(this).find("img");
-				var slideShow = $(this).next(".slide-show");
-				slideShow.append('<span class="bss-prev">«</span><span class="bss-next">»</span>');
+			gallerySlide : function(options) {
+				var thisObj = this;
+				var images = $(thisObj).find("img");
+				var slideShow = $("#slide-show");
 				var current = images.first();
-				var showImage = $('<img/>').attr('src', current.attr('src')).css('width','500px').css('height','500px');
+				
+				var showImage = $('<img/>').attr('src', current.attr('src'));
 				slideShow.find('figure').append(showImage);
-				$(this).show();
 				
 				var screenWidth = $(window).width(), screenHeight = $(window).height();  
 				var scrolltop = $(document).scrollTop();
@@ -15,28 +15,53 @@ $.fn.extend({
 				slideShow.css('top',top).css('left',left);
 				
 				
-				$.fn.gallerySlide.images = images;
-				$.fn.gallerySlide.index = 0;
 				slideShow.find('.bss-prev').click(function(){
-					if($.fn.gallerySlide.index == 0){
-						$.fn.gallerySlide.index = $.fn.gallerySlide.images.size()-1;
-					}else{
-						$.fn.gallerySlide.index = $.fn.gallerySlide.index-1;
-					}
-					var current = $($.fn.gallerySlide.images.get($.fn.gallerySlide.index));
+					var activeLiElement = thisObj.find('li.active');
+					activeLiElement.removeClass('active');
+					var newActiveLiElement = activeLiElement.prev();
 					
-					slideShow.find('img').attr('src', current.attr('src')).css('width','480px').css('height','490px');
+					var noMoreElement = newActiveLiElement.size()==0;
+					if(noMoreElement){
+						newActiveLiElement = thisObj.find('li').last();
+					}
+					newActiveLiElement.addClass('active');
+					slideShow.find('img').attr('src', newActiveLiElement.find('img').attr('src'));
 				});
 				
 				slideShow.find('.bss-next').click(function(){
-					if($.fn.gallerySlide.index == $.fn.gallerySlide.images.size()-1){
-						$.fn.gallerySlide.index = 0;
-					}else{
-						$.fn.gallerySlide.index = $.fn.gallerySlide.index+1;
-					}
-					var current = $($.fn.gallerySlide.images.get($.fn.gallerySlide.index));
+					var activeLiElement = thisObj.find('li.active');
+					activeLiElement.removeClass('active');
+					var newActiveLiElement = activeLiElement.next();
 					
-					slideShow.find('img').attr('src', current.attr('src')).css('width','500px').css('height','500px');
+					var noMoreElement = newActiveLiElement.size()==0;
+					if(noMoreElement){
+						newActiveLiElement = thisObj.find('li').first();
+					}
+					newActiveLiElement.addClass('active');
+					slideShow.find('img').attr('src', newActiveLiElement.find('img').attr('src'));
 				});
+				
+				slideShow.find('.bss-close').click(function(){
+					slideShow.hide();
+				});
+			},
+			
+			play : function(liElement) {
+				var thisObj = $(this);
+				if(!liElement){
+					liElement = thisObj.find('li').first();
+				}
+				thisObj.find('li.active').each(function(i,e){
+					$(e).removeClass('active');	
+				});
+				
+				liElement.addClass('active');
+
+				var allImages = $(thisObj).find("img");
+				var slideShow = $("#slide-show");
+				slideShow.find('img').attr('src', liElement.find('img').attr('src'));
+			
+				slideShow.show();
 			}
+			
 		});

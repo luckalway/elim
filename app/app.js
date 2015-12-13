5f53c8535cdeff46076d25044be758f0d9e69590
malachi.ye <malachi.ye@perficient.com>
2015-12-13 21:27:42 +0800

    add many pages

--- app/app.js
+++ app/app.js
@@ -7,6 +7,10 @@ app.config(function($routeProvider) {
 		templateUrl : 'app/item-detail/item-detail.html'
 	}).when('/index', {
 		templateUrl : 'app/home.html'
+	}).when('/aboutus', {
+		templateUrl : 'app/about-us/about-us.html'
+	}).when('/case', {
+		templateUrl : 'app/case/case.html'
 	}).otherwise({
 		redirectTo : '/index'
 	})

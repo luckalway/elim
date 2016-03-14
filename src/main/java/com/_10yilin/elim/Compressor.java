e3177a64e4178f110ede2a75a19d4eb281cbc269
malachi.ye <malachi.ye@perficient.com>
2016-03-14 18:30:25 +0800

    compress product images

--- src/main/java/com/_10yilin/elim/Compressor.java
+++ src/main/java/com/_10yilin/elim/Compressor.java
@@ -12,48 +12,76 @@ import org.imgscalr.Scalr;
 public class Compressor {
 	private static final Logger LOG = Logger.getLogger(Compressor.class);
 
-	private static final File OUT_PUT_FOLDER = new File("G:\\demo");
-
 	public static void main(String[] args) throws IOException {
-		File folder = new File("G:\\BaiduYunDownload");
-		check(folder);
+		final File outFolder = new File("G:\\demo");
+		File inFolder = new File("G:\\BaiduYunDownload");
+		process(inFolder, outFolder);
 	}
 
-	private static void check(File folder) throws IOException {
-		if (hasFile(folder, "flag")) {
-			traverseProcess(folder);
+	private static void process(File inFolder, File outFolder) throws IOException {
+		if (hasFile(inFolder, "flag")) {
+			traverseProcess(inFolder, outFolder);
 			return;
 		}
 
-		for (File child : folder.listFiles()) {
+		for (File child : inFolder.listFiles()) {
 			if (child.isFile()) {
-				LOG.warn("No flag file found for the path[" + folder.getAbsolutePath() + "]");
+				LOG.warn("No flag file found for the path[" + inFolder.getAbsolutePath() + "]");
 				break;
 			}
-			check(child);
+			process(child, outFolder);
 		}
 	}
 
-	private static void traverseProcess(File folder) throws IOException {
-		for (File product : folder.listFiles()) {
+	private static void traverseProcess(File inFolder, File outFolder) throws IOException {
+		for (File product : inFolder.listFiles()) {
 			if (product.isFile())
 				continue;
 
 			LOG.info("Start process product:" + product.getName());
-			for (File model : product.listFiles()) {
-				File target = new File(OUT_PUT_FOLDER, product.getName() + "/" + model.getName());
+			for (File sku : product.listFiles()) {
+				File target = new File(outFolder, product.getName() + "/" + sku.getName());
 				target.mkdirs();
+				LOG.info("Start process sku:" + sku.getName());
 
-				LOG.info("Start process model:" + model.getName());
-				for (File image : model.listFiles()) {
-					BufferedImage scaledImage = Scalr.resize(ImageIO.read(image), 750);
-					ImageIO.write(scaledImage, "jpg", new File(target, image.getName()));
+				if (sku.getName().equals("preview")) {
+					processPreviewImages(sku, target);
+					continue;
 				}
+				processGallery(sku, target);
 			}
 		}
 	}
 
-	public static boolean hasFile(File folder, String fileName) {
+	private static void processGallery(File sku, File target) throws IOException {
+		for (File originImage : sku.listFiles()) {
+			BufferedImage scaledImage = Scalr.resize(ImageIO.read(originImage), 750);
+			String newImageName = originImage.getName().toLowerCase();
+			File compressedImage = new File(target, newImageName);
+			ImageIO.write(scaledImage, "jpg", compressedImage);
+
+			BufferedImage scaled160Image = Scalr.resize(ImageIO.read(originImage), 160);
+			ImageIO.write(scaled160Image, "jpg", new File(target, newImageName + "_160x160.jpg"));
+
+			LOG.info(originImage.getAbsolutePath() + " ----> " + compressedImage.getAbsolutePath());
+		}
+	}
+
+	private static void processPreviewImages(File sku, File target) throws IOException {
+		for (File originImage : sku.listFiles()) {
+			BufferedImage scaledImage = Scalr.resize(ImageIO.read(originImage), 400);
+			String newImageName = originImage.getName().toLowerCase();
+			File compressedImage = new File(target, newImageName);
+			ImageIO.write(scaledImage, "jpg", compressedImage);
+
+			BufferedImage scaled160Image = Scalr.resize(ImageIO.read(originImage), 80);
+			ImageIO.write(scaled160Image, "jpg", new File(target, newImageName + "_80x80.jpg"));
+
+			LOG.info(originImage.getAbsolutePath() + " ----> " + compressedImage.getAbsolutePath());
+		}
+	}
+
+	private static boolean hasFile(File folder, String fileName) {
 		return new File(folder, fileName).exists();
 	}
 }

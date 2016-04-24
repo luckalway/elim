ca18367a821c6ce6fd28bfa3930a892d5a5393d8
malachi.ye <malachi.ye@perficient.com>
2016-04-24 22:08:10 +0800

    category bar

--- app/home-controller.js
+++ app/home-controller.js
@@ -2,6 +2,10 @@ app.controller('homeCtrl', function($scope, $routeParams) {
 	$scope.curtainItems = [];
 	for (var i = 0; i < 16; i++) {
 		$scope.curtainItems.push(window.curtainItems[i]);
-	}	
+	}
 	$scope.slideShows = window.slideShows;
+
+	$scope.toBuyiPage = function() {
+		window.location.href = '#buyi'
+	}
 });

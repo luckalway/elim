62960890ebb7fd9b05f65918304b056a820cd751
malachi.ye <malachi.ye@perficient.com>
2016-05-03 21:25:52 +0800

    home page

--- app/home-controller.js
+++ app/home-controller.js
@@ -1,11 +1,3 @@
 app.controller('homeCtrl', function($scope, $routeParams) {
-	$scope.curtainItems = [];
-	for (var i = 0; i < 16; i++) {
-		$scope.curtainItems.push(window.curtainItems[i]);
-	}
-	$scope.slideShows = window.slideShows;
 
-	$scope.toBuyiPage = function() {
-		window.location.href = '#buyi'
-	}
 });

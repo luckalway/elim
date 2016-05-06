761af26e73b9c7c70d3fe754604aba9703d0199b
malachi.ye <malachi.ye@perficient.com>
2016-05-06 09:15:35 +0800

    implement query feature

--- app/buyi/buyi-list-controller.js
+++ app/buyi/buyi-list-controller.js
@@ -30,37 +30,49 @@ app.controller('itemListCtrl', function($scope, $routeParams) {
 		$scope.pages.push(i);
 	}
 
-	$scope.filterItems = function(name, value) {
-		$scope.filterCurtainItems = [];
-		if(name == 'price'){
-			var minPrice = parseInt(value.split('-')[0]);
-			var maxPrice = parseInt(value.split('-')[1]);
-			
-			for(var i = 0; i < window.curtainItems.length; i++){
-				var item = window.curtainItems[i];
-				if(!isNaN(minPrice) && item.price < minPrice){
-					continue;	
+	$scope.query = {};
+	$scope.filterItems = function(name, value, label, valueDisplay) {
+		if(!(name == 'price' || name == 'craft' || name == 'style')){
+			return;
+		}
+		$scope.query[name] = {
+			label: label,
+			value : value,
+			display : valueDisplay
+		};console.dir($scope.query);
+		
+		var filterCurtainItems = window.curtainItems;
+		for(var name in $scope.query){
+			if(name == 'price'){
+				var minPrice = parseInt($scope.query[name].value.split('-')[0]);
+				var maxPrice = parseInt($scope.query[name].value.split('-')[1]);
+				
+				for(var i = 0; i < filterCurtainItems.length; i++){
+					var item = filterCurtainItems[i];
+					if(!isNaN(minPrice) && item.price < minPrice){
+						filterCurtainItems.remove(i);
+					}
+					if(!isNaN(maxPrice) && item.price >= maxPrice){
+						filterCurtainItems.remove(i);						
+					}
 				}
-				if(!isNaN(maxPrice) && item.price >= maxPrice){
-					continue;
+			}else if(name == 'craft'){
+				for(var i = 0; i < filterCurtainItems.length; i++){
+					var item = filterCurtainItems[i];
+					if(item.craft != $scope.query[name].value){
+						filterCurtainItems.remove(i);
+					}
 				}
-				$scope.filterCurtainItems.push(item);
-			}
-		}else if(name == 'craft'){
-			for(var i = 0; i < window.curtainItems.length; i++){
-				var item = window.curtainItems[i];
-				if(item.craft == value){
-					$scope.filterCurtainItems.push(item);
-				}
-			}
-		}else if(name == 'style'){
-			for(var i = 0; i < window.curtainItems.length; i++){
-				var item = window.curtainItems[i];
-				if(item.style == value){
-					$scope.filterCurtainItems.push(item);
+			}else if(name == 'style'){
+				for(var i = 0; i < filterCurtainItems.length; i++){
+					var item = filterCurtainItems[i];
+					if(item.style != $scope.query[name].value){
+						filterCurtainItems.remove(i);
+					}
 				}
 			}
 		}
+		$scope.filterCurtainItems = filterCurtainItems;
 		return false;
 	}
 })
